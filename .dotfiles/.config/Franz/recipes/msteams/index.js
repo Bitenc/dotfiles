dc98c9b2aeeff3f00020dff8d8d4a5873065f2e5
Thibault Bitenc <thibault.bitenc@tum.de>
2021-06-06 18:05:09 +0200

    deleted Franz and gtk configs and added new lines to install script

--- .dotfiles/.config/Franz/recipes/msteams/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = Franz => class MicrosoftTeams extends Franz {
-  overrideUserAgent() {
-    return window.navigator.userAgent.replace(/(Franz|Electron)([^\s]+\s)/g, '').replace(/(Chrome\/)([^ ]*)/g, '$163.0.3239.84');
-  }
-
-};
--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 stow .dotfiles
 
 touch ~/.localaliases
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+python .vim/plugged/YouCompleteMe/install.py
